9b861950c33d4535e5aad7b42a07e7dac0fb349d
sunandag <sunandag@lseg.com>
2018-07-30 13:27:25 +0530

    Changes to dummy build script

--- build_script.py
+++ build_script.py
@@ -1,16 +1,23 @@
 import sys
 
 build_dir = sys.argv[1]
-return_code = sys.argv[2]
+return_code = int(sys.argv[2])
+
+print("build_dir={}, return_code={}".format(build_dir, return_code))
 
 # Built success case: create some output file
 if return_code == 0:
+	print("Some output - 1")
 	print("Build script successful")
     
-	with open(build_dir + '/output_binary', "w") as output_file:
-		output_file.write('Binary_content')
+	with open(build_dir + '/output_binary_1', "w") as output_file:
+		output_file.write('Binary_content_1')
+		
+	with open(build_dir + '/output_binary_2', "w") as output_file:
+		output_file.write('Binary_content_2')
 
 else:
+	print("Some output - 2")
 	print("Build script failed", file=sys.stderr)
 
 
